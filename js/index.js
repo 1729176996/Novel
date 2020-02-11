2736c463e46af77e2abb6381c46f4ae8645d537c
1729176996 <1729176996@qq.com>
2020-02-11 16:53:23 +0800

    4

---
+++ js/index.js
@@ -0,0 +1,64 @@
+var vm,loading;
+$(function(){
+    FastClick.attach(document.body);
+	vm = new Vue({
+	    el: "#main",
+	    data:{
+			list:[]
+	    },
+	    mounted:function(){
+			this.init();
+			checkVersion();
+	    },
+	    methods:{
+			init:function(){
+				var _this = this;
+				_this.list = [
+					{
+						name:'小说',func:function(){
+							window.localStorage.setItem('type','all');
+							window.location.href = 'novel.html';
+						}
+					},
+					{
+						name:'历史上的今天',func:function(){
+							window.location.href = 'historyToday.html';
+						}
+					},
+					{
+						name:'笑话',func:function(){
+							window.location.href = 'joker.html';
+						}
+					},
+					{
+						name:'新闻',func:function(){
+							window.location.href = 'news.html';
+						}
+					},
+					{
+						name:'菜谱',func:function(){
+							window.location.href = 'recipe.html';
+						}
+					}
+				];
+				_this.$nextTick(function(){
+					mui('#scrollWrapper').scroll({
+						indicators:false,
+						deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+					}).scrollTo(0,0,0);
+				})
+			},
+			toTop:function(){
+				mui('#scrollWrapper').scroll({
+					indicators:false,
+					deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+				}).scrollTo(0,0,100);
+			},
+			selectItem:function(item){
+				if(item&&item.func){
+					item.func();
+				}
+			}
+	    }
+	});
+});
