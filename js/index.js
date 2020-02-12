b93b6cb60b3c8363320be80102cac09c3dc8ded3
1729176996 <1729176996@qq.com>
2020-02-12 14:45:09 +0800

    77

--- js/index.js
+++ js/index.js
@@ -4,141 +4,69 @@ $(function(){
 	vm = new Vue({
 	    el: "#main",
 	    data:{
-			list:[]
+			browser:''
 	    },
 	    mounted:function(){
 			this.init();
-			checkVersion();
 	    },
 	    methods:{
 			init:function(){
 				var _this = this;
-				_this.list = [
-					{
-						name:'小说',func:function(){
-							window.localStorage.setItem('type','all');
-							window.location.href = 'novel.html';
+				$.ajax({
+					url:'https://1729176996.github.io/MyH5AppVersion/version.json',
+					type:'get',
+					dataType: 'json',
+					success:function(data){
+						console.log(data);
+						var browser = getBrowser();
+						_this.browser = browser;
+						if(browser!='电脑端'){
+							// H5 plus事件处理
+							function plusReady(){
+								// 检查当前版本，与从后台获取的版本作比较，以此判断是否更新     
+								plus.runtime.getProperty(plus.runtime.appid,function(inf){
+									// 当前版本
+									var wgtVersion = inf.version;
+									// 如果有新版本，则提示需要更新
+									if( data.version > wgtVersion ){
+										var updatedContent = '';
+										if(data.updatedContent&&data.updatedContent.length>0){
+											updatedContent += '<p class="updatedContent">更新内容:</p><p class="updatedContent">';
+											for(key in data.updatedContent){
+												updatedContent += '<p class="updatedContent2">'+(key*1+1)+'.'+data.updatedContent[key]+'</p>';
+											}
+											updatedContent += '</p>';
+										}else{
+											updatedContent += '检查更新';
+										}
+										mui.confirm(updatedContent,'发现新版本，是否更新',['确定','取消'],function(e){
+											if(e.index==0){
+												downloadWgt(); // 下载wgt方法
+											}else{
+												window.location.href = 'shelf.html';
+												return;
+											}
+										},'div');
+									}else{
+										window.location.href = 'shelf.html';
+										return;
+									}
+								});
+							}
+							if(window.plus){
+								plusReady();
+							}else{
+								document.addEventListener('plusready',plusReady,false);
+							}
+						}else{
+							window.location.href = 'shelf.html';
+							return;
 						}
 					},
-					{
-						name:'历史上的今天',func:function(){
-							window.location.href = 'historyToday.html';
-						}
-					},
-					{
-						name:'笑话',func:function(){
-							window.location.href = 'joker.html';
-						}
-					},
-					{
-						name:'新闻',func:function(){
-							window.location.href = 'news.html';
-						}
-					},
-					{
-						name:'历史上的今天',func:function(){
-							window.location.href = 'historyToday.html';
-						}
-					},
-					{
-						name:'笑话',func:function(){
-							window.location.href = 'joker.html';
-						}
-					},
-					{
-						name:'新闻',func:function(){
-							window.location.href = 'news.html';
-						}
-					},
-					{
-						name:'历史上的今天',func:function(){
-							window.location.href = 'historyToday.html';
-						}
-					},
-					{
-						name:'笑话',func:function(){
-							window.location.href = 'joker.html';
-						}
-					},
-					{
-						name:'新闻',func:function(){
-							window.location.href = 'news.html';
-						}
-					},
-					{
-						name:'历史上的今天',func:function(){
-							window.location.href = 'historyToday.html';
-						}
-					},
-					{
-						name:'笑话',func:function(){
-							window.location.href = 'joker.html';
-						}
-					},
-					{
-						name:'新闻',func:function(){
-							window.location.href = 'news.html';
-						}
-					},
-					{
-						name:'历史上的今天',func:function(){
-							window.location.href = 'historyToday.html';
-						}
-					},
-					{
-						name:'笑话',func:function(){
-							window.location.href = 'joker.html';
-						}
-					},
-					{
-						name:'新闻',func:function(){
-							window.location.href = 'news.html';
-						}
-					},
-					{
-						name:'历史上的今天',func:function(){
-							window.location.href = 'historyToday.html';
-						}
-					},
-					{
-						name:'笑话',func:function(){
-							window.location.href = 'joker.html';
-						}
-					},
-					{
-						name:'新闻',func:function(){
-							window.location.href = 'news.html';
-						}
-					},
-					{
-						name:'菜谱',func:function(){
-							window.location.href = 'recipe.html';
-						}
+					error:function(xhr, errorType, error,msg){
+						mui.alert(msg,'提示','确定',null,'div');
 					}
-				];
-				_this.$nextTick(function(){
-					mui('#scrollWrapper').scroll({
-						indicators:false,
-						deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-					}).scrollTo(0,0,0);
 				})
-			},
-			toTop:function(){
-				mui('#scrollWrapper').scroll({
-					indicators:false,
-					deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-				}).scrollTo(0,0,100);
-			},
-			selectItem:function(item){
-				if(item&&item.func){
-					item.func();
-				}
-			},
-			toIndex:function(){
-				window.location.href = 'index.html';
-			},
-			toSearch:function(){
-				window.location.href = 'search.html';
 			}
 	    }
 	});
--- js/search.js
+++ js/search.js
@@ -5,8 +5,7 @@ $(function(){
 	    el: "#main",
 	    data:{
 			keyword:'',
-			list:[],
-			allList:[]
+			list:[]
 	    },
 	    mounted:function(){
 			this.init();
@@ -14,52 +13,59 @@ $(function(){
 	    methods:{
 			init:function(){
 				var _this = this;
-				var url = 'http://www.xbiquge.la/xiaoshuodaquan/';
-				loading = weui.loading("加载中");
-				$.ajax({
-					url:url,
-					type:'GET',
-					timeout:8000,
-					success:function(data){
-						
-						var list = data.split(/<div class="novellist">([\s\S]*?)<div class="clear">/);
-						var _list = [];
-						for(key in list){
-							var typeArr = list[key].match(/<h2>([\s\S]*?)<\/h2>/);
-							var type = typeArr&&(typeArr.length==2)?typeArr[1]:'';
-							if(type){
-								var li_list = list[key].split(/<li>([\s\S]*?)<\/li>/);
-								for(k in li_list){
-									var arr = li_list[k].split(/<a href="([\s\S]*?)">([\s\S]*?)<\/a>/);
-									if(arr&&arr.length==4){
-										var o = {
-											name:arr[2],
-											href:arr[1]
-										};
-										_list.push(o)
-									}
-								}
-							}
-						}
-						_this.allList = _list;
-						_this.list = _list;
-						_this.$nextTick(function(){
-							loading.hide();
-							mui('#scrollWrapper').scroll({
-								indicators:false,
-								deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-							}).scrollTo(0,0,0);
-						})
-					},
-					error:function(xhr, errorType, error,msg){
-						mui.alert(msg,'提示','确定',null,'div');
-						loading.hide();
-					}
+				_this.list = [];
+				_this.$nextTick(function(){
+					loading.hide();
+					mui('#scrollWrapper').scroll({
+						indicators:false,
+						deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+					}).scrollTo(0,0,0);
 				})
 			},
 			search:function(){
 				var _this = this;
-				
+				var url = 'https://qxs.la/s_'+_this.keyword+'/1/';
+				loading = weui.loading("加载中");
+				$.ajax({
+					url:url,
+					type:'GET',
+					dataType:'json',
+					timeout:8000,
+					success:function(data){
+						console.log(data);
+						/*if(data){
+							if(data.code == "10000"){
+								if(data.result){
+									if(data.result.result&&data.result.result&&data.result.result.list&&data.result.result.list.length>0){
+										_this.list = data.result.result.list;
+									}
+								}else{
+									mui.alert('查询错误','提示','确定',null,'div');
+								}
+							}else{
+								mui.alert(data.msg,'提示','确定',null,'div');
+							}
+						}else{
+							mui.alert('查询错误','提示','确定',null,'div');
+						}*/
+						_this.$nextTick(function(){
+							mui('#scrollWrapper').scroll({
+								indicators:false,
+								deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+							}).scrollTo(0,0,0);
+							
+							if(loading){
+								loading.hide();
+							}
+						})
+					},
+					error:function(xhr, errorType, error,msg){
+						if(loading){
+							loading.hide();
+						}
+						mui.alert(msg,'提示','确定',null,'div');
+					}
+				})
 			},
 			toTop:function(){
 				mui('#scrollWrapper').scroll({
