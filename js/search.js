6727a46987be1c3a5b704c3d51ef7544d3aadc10
1729176996 <1729176996@qq.com>
2020-02-13 12:13:58 +0800

    5

--- js/search.js
+++ js/search.js
@@ -15,7 +15,7 @@ $(function(){
 				var _this = this;
 				_this.list = [];
 				_this.$nextTick(function(){
-					loading.hide();
+					
 					mui('#scrollWrapper').scroll({
 						indicators:false,
 						deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
@@ -29,10 +29,42 @@ $(function(){
 				$.ajax({
 					url:url,
 					type:'GET',
-					dataType:'json',
 					timeout:8000,
 					success:function(data){
-						console.log(data);
+						
+						var _list = [];
+						var uls = data.split(/<ul class="list_content">([\s\S]*?)<\/ul>/);
+						for(var n=1;n<uls.length;n++){
+							var lis = uls[n].split(/<li class=([\s\S]*?)<\/li>/);
+							//console.log(lis);
+							if(lis.length==9){
+								var as1 = lis[1].match(/<a href="([\s\S]*?)">([\s\S]*?)<\/a>/);
+								var url = as1[1];
+								var name = as1[2];
+								
+								var as2 = lis[3].match(/<a href="([\s\S]*?)">([\s\S]*?)<\/a>/);
+								var last_url = as2[1];
+								var last_title = as2[2];
+								
+								var as3 = lis[5].match(/<a href="([\s\S]*?)">([\s\S]*?)<\/a>/);
+								var author = as3[2];
+								
+								var as4 = lis[7].split(/"cc5">/);
+								var last_time = as4[1];
+								
+								var obj = {
+									name:name,
+									url:url,
+									last_title:last_title,
+									last_url:last_url,
+									author:author,
+									last_time:last_time
+								};
+								_list.push(obj);
+							}
+						}
+						_this.list = _list;
+						//console.log(list);
 						/*if(data){
 							if(data.code == "10000"){
 								if(data.result){
